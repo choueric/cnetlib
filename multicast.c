fd406e98218c1a00ce2e9748bed995bfe2099781
choueric <zhssmail@gmail.com>
2016-12-26 15:10:11 +0800

    rework 'multicast'

--- multicast.c
+++ multicast.c
@@ -6,6 +6,7 @@
 
 #include "multicast.h"
 #include "address.h"
+#include "log.h"
 
 // modified from source code of net-tools_1.60, ipmaddr.c
 // NOTE: ONLY support IPv4
@@ -13,6 +14,9 @@
 #define PATH_PROCNET_IGMP		"/proc/net/igmp"      /* net/core/igmp.c */
 #define PATH_PROCNET_DEV_MCAST  "/proc/net/dev_mcast" /* net/core/dev_addr_lists.c */
 
+// parse hex number in string format form @str and store in @addr.
+// @return: the number of byte stored in @addr.
+// e.g. "c0a80001" -> addr[4] = {1, 0, 168, 192}
 static int parse_hex(char *str, unsigned char *addr)
 {
 	int len = 0;
@@ -43,7 +47,7 @@ static int read_dev_mcast(struct mcast_info *array, int num)
 
 	FILE *fp = fopen(PATH_PROCNET_DEV_MCAST, "r");
 	if (!fp) {
-        perror("fopen failed");
+        err("fopen %s failed: %m\n", PATH_PROCNET_DEV_MCAST);
 		return -1;
     }
 
@@ -57,14 +61,14 @@ static int read_dev_mcast(struct mcast_info *array, int num)
 		sscanf(buf, "%d%s%d%d%s", &m->index, m->name, &m->users, &m->st, hexa);
 		len = parse_hex(hexa, (unsigned char*)m->addr);
         if (len < 0) {
-            printf("parse hex failed\n");
+            err("parse hex failed\n");
             return -1;
         }
 
 		m->family = AF_PACKET;
         i++;
         if (i >= num) {
-            printf("over num %d\n", num);
+            err("over num %d\n", num);
             fclose(fp);
             return -1;
         }
@@ -88,11 +92,11 @@ static int read_igmp(struct mcast_info *array, int num)
 
 	FILE *fp = fopen(PATH_PROCNET_IGMP, "r");
 	if (!fp) {
-        perror("fopen failed");
+        err("fopen %s failed: %m\n", PATH_PROCNET_IGMP);
 		return -1;
     }
 	if (fgets(buf, sizeof(buf), fp) == NULL)  // eat first line
-		printf("fgets failed\n");
+		err("fgets failed\n");
 
     i = 0;
     bzero(array, sizeof(struct mcast_info) * num);
@@ -109,10 +113,10 @@ static int read_igmp(struct mcast_info *array, int num)
             m->index = head->index;
             strncpy(m->name, head->name, IFNAMSIZ);
         }
-		sscanf(buf, "%08x%d", m->addr, &m->users);
+		sscanf(buf, "%08x%d", (unsigned int*)m->addr, &m->users);
         i++;
         if (i >= num) {
-            printf("over num %d\n", num);
+            err("over num %d\n", num);
             fclose(fp);
             return -1;
         }
@@ -122,8 +126,137 @@ static int read_igmp(struct mcast_info *array, int num)
     return i;
 }
 
-static void mcast_print(struct mcast_info *m, FILE *fp)
+// normal group, non-source group
+static int mcast_join_leave_inet(int skfd, char *ifname, struct sockaddr *grp, int join)
 {
+    if (grp->sa_family != AF_INET) {
+        err("Not support non-AF_INET mcast ops\n");
+        return -1;
+    }
+
+    int ret = 0;
+    struct ip_mreq mreq;
+    int cmd = (join ? IP_ADD_MEMBERSHIP : IP_DROP_MEMBERSHIP);
+
+    memcpy(&mreq.imr_multiaddr, &((struct sockaddr_in *)grp)->sin_addr,
+            sizeof(struct in_addr));
+
+    struct ifreq ifreq;
+    strncpy(ifreq.ifr_name, ifname, IFNAMSIZ);
+    if (ioctl(skfd, SIOCGIFADDR, &ifreq) < 0) {
+        err("get ifaddr failed: %m");
+        return -1;
+    }
+    memcpy(&mreq.imr_interface, &((struct sockaddr_in *)&ifreq.ifr_addr)->sin_addr,
+            sizeof(struct in_addr));
+
+    ret = setsockopt(skfd, IPPROTO_IP, cmd, &mreq, sizeof(mreq));
+    if (ret < 0) {
+        err("%s multicast ioctl failed: %m", join ? "join": "leave");
+        return -1;
+    }
+    return 0;
+}
+
+
+static int mcast_set_ttl_inet(int skfd, int val)
+{
+    unsigned char ttl = val;
+    int ret = setsockopt(skfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
+    if (ret < 0) {
+        err("set multicast TTL failed: %m");
+        return -1;
+    }
+    return 0;
+}
+
+static int mcast_get_ttl_inet(int skfd)
+{
+    unsigned char ttl;
+    socklen_t len;
+
+    len = sizeof(ttl);
+    if (getsockopt(skfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, &len) < 0) {
+        err("get multicast TTL failed: %m\n");
+        return -1;
+    }
+    return ttl;
+}
+
+static int mcast_set_iface_inet(int skfd, char *ifname)
+{
+    struct in_addr inaddr;
+    struct ifreq ifreq;
+
+    strncpy(ifreq.ifr_name, ifname, IFNAMSIZ);
+    if (ioctl(skfd, SIOCGIFADDR, &ifreq) < 0) {
+        err("get iface addr ioctl failed: %m\n");
+        return -1;
+    }
+    memcpy(&inaddr, &((struct sockaddr_in *)&ifreq.ifr_addr)->sin_addr,
+            sizeof(struct in_addr));
+
+    int ret = setsockopt(skfd, IPPROTO_IP, IP_MULTICAST_IF,
+                &inaddr, sizeof(struct in_addr));
+    if (ret < 0) {
+        err("set multicast iface failed: %m\n");
+        return -1;
+    }
+    return 0;
+}
+
+static int mcast_get_iface_inet(int skfd, uint32_t *ip)
+{
+    struct in_addr inaddr;
+    socklen_t len;
+
+    int ret = getsockopt(skfd, IPPROTO_IP, IP_MULTICAST_IF,
+                &inaddr, &len);
+    if (ret < 0) {
+        *ip = 0;
+        err("set multicast iface failed: %m\n");
+        return -1;
+    }
+    info("ip = %08x\n", inaddr.s_addr);
+    *ip = ntohl(inaddr.s_addr);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int mcast_get_list(struct mcast_info *array, int size, int family)
+{
+    int n = 0;
+	int ret;
+
+	if (family == AF_PACKET || family == AF_UNSPEC) {
+		ret = read_dev_mcast(array, size);
+		if (ret < 0) {
+			err("read_dev_mcast failed\n");
+			return -1;
+		}
+		n += ret;
+	}
+
+	if (family == AF_INET || family == AF_UNSPEC) {
+		ret = read_igmp(array + n, size - n);
+		if (ret < 0) {
+			err("read_igmp failed\n");
+			return -2;
+		}
+		n += ret;
+	}
+
+    return n;
+}
+
+void mcast_print(struct mcast_info *m, FILE *fp)
+{
+    if (m == NULL || fp == NULL) {
+        err("Invliad parameters\n");
+        return;
+    }
+
     char str[32] = {0};
     const char *type = NULL;
 
@@ -142,139 +275,13 @@ static void mcast_print(struct mcast_info *m, FILE *fp)
     else
         type = "?";
 
-    fprintf(fp, "%3d %s: %4s %-18s [%d]\n", m->index, m->name, type, str, m->users);
+    fprintf(fp, "%3d %-6s: %4s %-18s [%d]\n", m->index, m->name, type, str, m->users);
 }
 
-
-// normal group, non-source group
-static int mcast_join_leave_inet(int skfd, char *ifname, struct sockaddr *grp, int join)
-{
-    if (grp->sa_family != AF_INET) {
-        printf("Not support non-AF_INET mcast ops\n");
-        return -1;
-    }
-
-    int ret = 0;
-    struct ip_mreq mreq;
-    int cmd = (join ? IP_ADD_MEMBERSHIP : IP_DROP_MEMBERSHIP);
-
-    memcpy(&mreq.imr_multiaddr, &((struct sockaddr_in *)grp)->sin_addr,
-            sizeof(struct in_addr));
-
-    struct ifreq ifreq;
-    strncpy(ifreq.ifr_name, ifname, IFNAMSIZ);
-    if (ioctl(skfd, SIOCGIFADDR, &ifreq) < 0) {
-        perror("get ifaddr failed");
-        return -1;
-    }
-    memcpy(&mreq.imr_interface, &((struct sockaddr_in *)&ifreq.ifr_addr)->sin_addr,
-            sizeof(struct in_addr));
-
-    ret = setsockopt(skfd, IPPROTO_IP, cmd, &mreq, sizeof(mreq));
-    if (ret < 0) {
-        perror("join/leave multicast ioctl failed");
-        return -1;
-    }
-    return 0;
-}
-
-
-static int mcast_set_ttl_inet(int skfd, int val)
-{
-    unsigned char ttl = val;
-    int ret = setsockopt(skfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl));
-    if (ret < 0) {
-        perror("set multicast TTL failed");
-        return -1;
-    }
-    return 0;
-}
-
-static int mcast_get_ttl_inet(int skfd)
-{
-    unsigned char ttl;
-    socklen_t len;
-
-    len = sizeof(ttl);
-    if (getsockopt(skfd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, &len) < 0) {
-        perror("get multicast TTL failed");
-        return -1;
-    }
-    return ttl;
-}
-
-static int mcast_set_iface_inet(int skfd, char *ifname)
-{
-    struct in_addr inaddr;
-    struct ifreq ifreq;
-
-    strncpy(ifreq.ifr_name, ifname, IFNAMSIZ);
-    if (ioctl(skfd, SIOCGIFADDR, &ifreq) < 0) {
-        perror("get iface addr ioctl failed");
-        return -1;
-    }
-    memcpy(&inaddr, &((struct sockaddr_in *)&ifreq.ifr_addr)->sin_addr,
-            sizeof(struct in_addr));
-
-    int ret = setsockopt(skfd, IPPROTO_IP, IP_MULTICAST_IF,
-                &inaddr, sizeof(struct in_addr));
-    if (ret < 0) {
-        perror("set multicast iface failed");
-        return -1;
-    }
-    return 0;
-}
-
-static int mcast_get_iface_inet(int skfd, uint32_t *ip)
-{
-    struct in_addr inaddr;
-    socklen_t len;
-
-    int ret = getsockopt(skfd, IPPROTO_IP, IP_MULTICAST_IF,
-                &inaddr, &len);
-    if (ret < 0) {
-        *ip = 0;
-        perror("set multicast iface failed");
-        return -1;
-    }
-    printf("ip = %08x\n", inaddr.s_addr);
-    *ip = ntohl(inaddr.s_addr);
-    return 0;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-/*
- * @family: 1: ipv4 (AF_INET)
- *          2: link (AF_PACKET)
- *          3: all (AF_UNSEPC)
- */
-int MM_mcast_get_list(struct mcast_info *array, int size, int family)
-{
-    int n = 0;
-
-	if (family == AF_PACKET || family == AF_UNSPEC)
-		n = read_dev_mcast(array, size);
-
-	if (family == AF_INET || family == AF_UNSPEC)
-		n += read_igmp(array + n, size - n);
-
-    return n;
-}
-
-void MM_mcast_print(struct mcast_info *m, FILE *fp)
-{
-    if (m == NULL || fp == NULL) {
-        printf("Invliad parameters\n");
-        return;
-    }
-    mcast_print(m, fp);
-}
-
-int MM_mcast_join(int skfd, char *ifname, uint32_t ip)
+int mcast_join(int skfd, char *ifname, uint32_t ip)
 {
     if (skfd <= 0 || ifname == NULL || ip == 0) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
@@ -285,10 +292,10 @@ int MM_mcast_join(int skfd, char *ifname, uint32_t ip)
     return mcast_join_leave_inet(skfd, ifname, &grp, true);
 }
 
-int MM_mcast_leave(int skfd, char *ifname, uint32_t ip)
+int mcast_leave(int skfd, char *ifname, uint32_t ip)
 {
     if (skfd <= 0 || ifname == NULL || ip == 0) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
@@ -302,43 +309,40 @@ int MM_mcast_leave(int skfd, char *ifname, uint32_t ip)
 /*
  * actually, in IPv4, @ttl's type is u_char
  */
-int MM_mcast_set_ttl(int skfd, int ttl)
+int mcast_set_ttl(int skfd, int ttl)
 {
     if (skfd <= 0 || ttl < 0) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
     return mcast_set_ttl_inet(skfd, ttl);
 }
 
-/*
- * actually, in IPv4, TTL's type is u_char
- */
-int MM_mcast_get_ttl(int skfd)
+int mcast_get_ttl(int skfd)
 {
     if (skfd <= 0) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
     return mcast_get_ttl_inet(skfd);
 }
 
-int MM_mcast_set_iface(int skfd, char *ifname)
+int mcast_set_iface(int skfd, char *ifname)
 {
     if (skfd <= 0 || ifname == NULL) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
     return mcast_set_iface_inet(skfd, ifname);
 }
 
-int MM_mcast_get_iface(int skfd, uint32_t *ip)
+int mcast_get_iface(int skfd, uint32_t *ip)
 {
     if (skfd <= 0 || ip == NULL) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
@@ -371,37 +375,5 @@ int mcast_modify_ether(char *ifname, int is_add, char *hwaddr, int len)
         return -1;
     }
     close(fd);
-    return 0;
-}
-
-int mcast_leave_inet(int skfd, struct sockaddr *grp)
-{
-    if (grp->sa_family != AF_INET) {
-        printf("Not support non-AF_INET mcast ops\n");
-        return -1;
-    }
-
-    int ret = 0;
-    struct ip_mreq mreq;
-
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-
-    struct ifreq ifreq;
-    strncpy(ifreq.ifr_name, "eth0", IFNAMSIZ);
-    if (ioctl(skfd, SIOCGIFADDR, &ifreq) < 0) {
-        perror("get ifaddr failed");
-        return -1;
-    }
-    memcpy(&mreq.imr_interface, &((struct sockaddr_in *)&ifreq.ifr_addr)->sin_addr,
-            sizeof(struct in_addr));
-
-    memcpy(&mreq.imr_multiaddr, &((struct sockaddr_in *)grp)->sin_addr,
-            sizeof(struct in_addr));
-
-    ret = (setsockopt(skfd, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq)));
-    if (ret < 0) {
-        perror("leave multicast ioctl failed");
-        return -1;
-    }
     return 0;
 }
--- multicast.h
+++ multicast.h
@@ -7,6 +7,60 @@ struct mcast_info {
 	int users;
 	int st;
 	int index;
-	int addr[6];
+	unsigned char addr[6];
 	sa_family_t family;
 };
+
+/*
+ * get the multicast list for IPv4 and/or Link layer
+ *
+ * @array, @size: to store the restults.
+ * @family: AF_INET for IPv4; AF_PACKET for Link; AF_UNSPEC for both.
+ * @return: >= 0, the number of entries returned; < 0, failed.
+ */
+int mcast_get_list(struct mcast_info *array, int size, int family);
+
+/*
+ * print a mcast_info entry.
+ * format: "iface index, iface name: type, address, users"
+ */
+void mcast_print(struct mcast_info *m, FILE *fp);
+
+/*
+ * join a multicast group
+ *
+ * @skfd: socket fd
+ * @ifname: interface name
+ * @ip: group IP
+ * @return: 0, success; <0 fail.
+ */
+int mcast_join(int skfd, char *ifname, uint32_t ip);
+
+/*
+ * leave a multicast group
+ *
+ * @skfd: socket fd
+ * @ifname: interface name
+ * @ip: group IP
+ * @return: 0, success; <0 fail.
+ */
+int mcast_leave(int skfd, char *ifname, uint32_t ip);
+
+/*
+ * set TTL
+ *
+ * in IPv4, type of TTL is u_char
+ *
+ * @skfd: socket fd
+ * @ttl: TLL value to set
+ * @return: 0, success; others, fail.
+ */
+int mcast_set_ttl(int skfd, int ttl);
+
+/*
+ * return TLL.
+ *
+ * @skfd: socket fd
+ * @return: value TTL; < 0, fail.
+ */
+int mcast_get_ttl(int skfd);
