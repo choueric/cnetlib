91aa0044647c516e240ff358c6542f4b849fa5d5
choueric <zhssmail@gmail.com>
2016-12-26 15:40:17 +0800

    rework route module

--- route.c
+++ route.c
@@ -1,4 +1,5 @@
 #include <sys/ioctl.h>
+#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <net/if.h>
@@ -14,6 +15,7 @@
 
 #include "route.h"
 #include "address.h"
+#include "log.h"
 
 /*
  * modified from source code of net-tools_1.60, route.c
@@ -28,30 +30,13 @@
 #define RT_ACTION_ADD 1
 #define RT_ACTION_DEL 0
 
-#if 0  // comments
-
-/* from kernel, include/uapi/linux/route.h */
-#define	RTF_UP	    	0x0001		/* route usable		  	*/
-#define	RTF_GATEWAY 	0x0002		/* destination is a gateway	*/
-#define	RTF_HOST    	0x0004		/* host entry (net otherwise)	*/
-#define RTF_REINSTATE	0x0008		/* reinstate route after tmout	*/
-#define	RTF_DYNAMIC 	0x0010		/* created dyn. (by redirect)	*/
-#define	RTF_MODIFIED	0x0020		/* modified dyn. (by redirect)	*/
-#define RTF_MTU	    	0x0040		/* specific MTU for this route	*/
-#define RTF_MSS	    	RTF_MTU		/* Compatibility :-(		*/
-#define RTF_WINDOW  	0x0080		/* per route window clamping	*/
-#define RTF_IRTT    	0x0100		/* Initial round trip time	*/
-#define RTF_REJECT  	0x0200		/* Reject route			*/
-
-#endif
-
 static inline void set_addr_family(struct sockaddr *sa, uint32_t addr, sa_family_t f)
 {
     sa_set_family(sa, f);
     sa_set_addr(sa, addr);
 }
 
-static void route_set_entry(struct nl_rtentry *ent, char *iface, 
+static void _set_entry(struct nl_rtentry *ent, char *iface, 
         uint32_t dst, uint32_t gw, unsigned short flags,
         short metric, uint32_t mask, unsigned long mtu,
         unsigned long window, unsigned long irtt)
@@ -67,12 +52,12 @@ static void route_set_entry(struct nl_rtentry *ent, char *iface,
     ent->rt_irtt = irtt;
 }
 
-static uint32_t route_str2u32(const char *str)
+static uint32_t _str2u32(const char *str)
 {
     uint32_t i = 0;
     int ret = sscanf(str, "%08x", &i);
     if (ret != 1) {
-        printf("sscanf for IP address failed\n");
+        err("sscanf for IP address failed\n");
         return 0;
     }
     return ntohl(i);
@@ -80,11 +65,8 @@ static uint32_t route_str2u32(const char *str)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/*
- * @return: if succeed, return the actual number of route entry
- * fib_route_seq_show() in net/ipv4/fib_trie.c
- */
-static int route_get_list(struct nl_rtentry *array, int size)
+/* fib_route_seq_show() in net/ipv4/fib_trie.c */
+static int _route_get_list(struct nl_rtentry *array, int size)
 {
     char buff[512], iface[IFNAMSIZ];
     char gate_addr[32], net_addr[32], mask_addr[32];
@@ -95,14 +77,14 @@ static int route_get_list(struct nl_rtentry *array, int size)
 
     FILE *fp = fopen(PATH_PROCNET_ROUTE, "r");
     if (!fp) {
-        perror(PATH_PROCNET_ROUTE);
+        err("open %s failed: %m\n", PATH_PROCNET_ROUTE);
         return -1;
     }
 
     bzero(array, sizeof(struct nl_rtentry) * size);
 
     if (!fgets(buff, sizeof(buff) - 1, fp)) {
-        printf("eat first line failed\n");
+        err("eat first line failed\n");
         return -1;
     }
 
@@ -113,19 +95,19 @@ static int route_get_list(struct nl_rtentry *array, int size)
                 &iflags, &refcnt, &use, &metric, mask_addr,
                 &mtu, &window, &irtt);
         if (ret < 10 || !(iflags & RTF_UP)) {
-            printf("warning: faile to parse or unusable route entry: %s\n", buff);
+            err("warning: faile to parse or unusable route entry: %s\n", buff);
             continue;
         }
 
-        snet_target = route_str2u32(net_addr);
-        snet_gateway = route_str2u32(gate_addr);
-        snet_mask = route_str2u32(mask_addr);
+        snet_target = _str2u32(net_addr);
+        snet_gateway = _str2u32(gate_addr);
+        snet_mask = _str2u32(mask_addr);
 
-        route_set_entry(array + i, iface, snet_target, snet_gateway, 
+        _set_entry(array + i, iface, snet_target, snet_gateway, 
                 iflags, metric, snet_mask, mtu, window, irtt);
         i++;
         if (i >= size) {
-            printf("over %d max rt entry number\n", size);
+            err("over %d max rt entry number\n", size);
             fclose(fp);
             return -1;
         }
@@ -140,15 +122,14 @@ static int do_route_modify(struct rtentry *rt, int action)
     int ret = 0;
     int skfd = 0;
 
-    /* Tell the kernel to accept this route. */
     if ((skfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket");
+        err("create socket fail: %m\n");
         return -1;
     }
 
     int cmd = ((action == RT_ACTION_DEL) ? SIOCDELRT : SIOCADDRT);
     if (ioctl(skfd, cmd, rt) < 0) {
-        perror("rt ioctl failed");
+        err("rt ioctl failed: %m\n");
         ret = -1;
     }
     close(skfd);
@@ -202,7 +183,7 @@ static int route_modify(int action, int is_net,
 
     if (mtu != NULL) {
         if (*mtu < 64 || *mtu > 65536) {
-            fprintf(stderr, "route: Invalid MSS/MTU.\n");
+            err("route: Invalid MSS/MTU.\n");
             return -1;
         }
         rt.rt_mss = *mtu;
@@ -210,7 +191,7 @@ static int route_modify(int action, int is_net,
     }
     if (window != NULL) {
         if (*window < 128) {
-            fprintf(stderr, "route: Invalid window.\n");
+            err("route: Invalid window.\n");
             return -1;
         }
         rt.rt_flags |= RTF_WINDOW;
@@ -221,16 +202,16 @@ static int route_modify(int action, int is_net,
     if (genmask_in_addr(rt)) {
         uint32_t mask = ~ntohl(genmask_in_addr(rt));
         if ((rt.rt_flags & RTF_HOST) && mask != 0xffffffff) {
-            fprintf(stderr, "netmask %.8x doesn't useful when set host route\n", mask);
+            err("netmask %.8x doesn't useful when set host route\n", mask);
             return -1;
         }
         if (mask & (mask + 1)) {
-            fprintf(stderr, "route: invalid netmask 0x%x\n", *netmask);
+            err("route: invalid netmask 0x%x\n", *netmask);
             return -1;
         }
         uint32_t dst_net = ((struct sockaddr_in *) &rt.rt_dst)->sin_addr.s_addr;
         if (dst_net & ~genmask_in_addr(rt)) {
-            fprintf(stderr, "route: netmask doesn't match route address\n");
+            err("route: netmask doesn't match route address\n");
             return -1;
         }
     }
@@ -242,46 +223,87 @@ static int route_modify(int action, int is_net,
     return do_route_modify(&rt, action);
 }
 
-static void route_print_entry(FILE *fp, struct nl_rtentry *ent)
+// flags of route entry. from kernel, include/uapi/linux/route.h
+// #define RTF_UP	    	0x0001		/* route usable		  	*/
+// #define RTF_GATEWAY 	0x0002		/* destination is a gateway	*/
+// #define RTF_HOST    	0x0004		/* host entry (net otherwise)	*/
+// #define RTF_REINSTATE	0x0008		/* reinstate route after tmout	*/
+// #define RTF_DYNAMIC 	0x0010		/* created dyn. (by redirect)	*/
+// #define RTF_MODIFIED	0x0020		/* modified dyn. (by redirect)	*/
+// #define RTF_MTU	    	0x0040		/* specific MTU for this route	*/
+// #define RTF_MSS	    	RTF_MTU		/* Compatibility :-(		*/
+// #define RTF_WINDOW  	0x0080		/* per route window clamping	*/
+// #define RTF_IRTT    	0x0100		/* Initial round trip time	*/
+// #define RTF_REJECT  	0x0200		/* Reject route			*/
+static void print_flags(FILE *fp, uint16_t flags)
+{
+    if (flags == 0)
+        fprintf(fp, "[NO FLAGS] ");
+	if (flags & RTF_UP)
+		fprintf(fp, "UP ");
+	if (flags & RTF_GATEWAY)
+		fprintf(fp, "GATEWAY ");
+	if (flags & RTF_HOST)
+		fprintf(fp, "HOST ");
+	if (flags & RTF_REINSTATE)
+		fprintf(fp, "REINSTATE ");
+	if (flags & RTF_DYNAMIC)
+		fprintf(fp, "DYNAMIC ");
+	if (flags & RTF_MODIFIED)
+		fprintf(fp, "MODIFIED ");
+	if (flags & RTF_MTU)
+		fprintf(fp, "MTU ");
+	if (flags & RTF_MSS)
+		fprintf(fp, "MSS ");
+	if (flags & RTF_WINDOW)
+		fprintf(fp, "WINDOW ");
+	if (flags & RTF_IRTT)
+		fprintf(fp, "IRTT ");
+	if (flags & RTF_REJECT)
+		fprintf(fp, "REJECT ");
+}
+
+
+
+static void _route_print_entry(FILE *fp, struct nl_rtentry *ent)
 {
     char str[32];
-    fprintf(fp, "dst: %16s ", sa_itos(ent->rt_dst, str, sizeof(str)));
-    fprintf(fp, "gw: %16s ", sa_itos(ent->rt_gateway, str, sizeof(str)));
-    fprintf(fp, "genmask: %16s ", sa_itos(ent->rt_genmask, str, sizeof(str)));
-    fprintf(fp, "iface: %s  flags: 0x%x  metric: %d  mtu: %lu\n",
-            ent->rt_dev, ent->rt_flags, ent->rt_metric, ent->rt_mtu);
+    fprintf(fp, "dst: %-16s ", sa_itos(ent->rt_dst, str, sizeof(str)));
+    fprintf(fp, "gw: %-16s ", sa_itos(ent->rt_gateway, str, sizeof(str)));
+    fprintf(fp, "genmask: %-16s ", sa_itos(ent->rt_genmask, str, sizeof(str)));
+    fprintf(fp, "iface: %s,  flags: ", ent->rt_dev);
+	print_flags(fp, ent->rt_flags);
+	fprintf(fp, "metric: %d,  mtu: %lu.\n", ent->rt_metric, ent->rt_mtu);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-int MM_route_get_list(struct nl_rtentry *array, int size)
+int route_get_list(struct nl_rtentry *array, int size)
 {
     if (array == NULL || size <= 0) {
-        printf("invalid parameters\n");
+        err("invalid parameters\n");
         return -1;
     }
 
-    return route_get_list(array, size);
+    return _route_get_list(array, size);
 }
 
-/*
- * @gw, @mask, @dev: if use default value, just pass NULL
- */
-int MM_route_add(bool is_net, uint32_t dst, uint32_t *gw, uint32_t *mask, char *dev)
+void route_print_entry(struct nl_rtentry *rt, FILE *fp)
+{
+    if (rt == NULL || fp == NULL) {
+        err("invalid parameter\n");
+        return;
+    }
+    _route_print_entry(fp, rt);
+}
+
+int route_add(int is_net, uint32_t dst, uint32_t *gw, uint32_t *mask, char *dev)
 {
     return route_modify(RT_ACTION_ADD, is_net, dst, gw, mask, dev, NULL, NULL, NULL);
 }
 
-int MM_route_del(bool is_net, uint32_t dst, uint32_t *gw, uint32_t *mask, char *dev)
+int route_del(int is_net, uint32_t dst, uint32_t *gw, uint32_t *mask, char *dev)
 {
     return route_modify(RT_ACTION_DEL, is_net, dst, gw, mask, dev, NULL, NULL, NULL);
 }
 
-void MM_route_print_entry(struct nl_rtentry *rt, FILE *fp)
-{
-    if (rt == NULL || fp == NULL) {
-        printf("invalid parameter\n");
-        return;
-    }
-    route_print_entry(fp, rt);
-}
--- route.h
+++ route.h
@@ -13,3 +13,41 @@ struct nl_rtentry {
     unsigned long rt_window;
     unsigned long rt_irtt;
 };
+
+/*
+ * get list of route entries.
+ *
+ * @array, @size: array to store route entries.
+ * @return: <0, fail; others, the number of route entries, 
+ */
+int route_get_list(struct nl_rtentry *array, int size);
+
+void route_print_entry(struct nl_rtentry *rt, FILE *fp);
+
+/*
+ * add route entry.
+ * "route add -net 192.56.76.0 netmask 255.255.255.0 dev eth0"
+ * "route add -net 127.0.0.0 netmask 255.0.0.0 dev lo"
+ * "route del default"
+ * "route add default gw 192.168.0.254"
+ * "route del -net 172.0.0.0 netmask 255.255.255.0 dev eth0"
+ *
+ * if @gw is 0.0.0.0, it means the packet for @dst is sent through @dev 
+ * directly. default value of @gateway is 0.0.0.0
+ *
+ * @is_net: 1, @dst is subnet address; 0, @dst is host address.
+ * @dst: destination IP address. default is 0.0.0.0
+ * @gw: gateway for this route.
+ * @mask: network of the destination network.
+ * @dev: interface name.
+ *
+ * @gw, @mask, @dev: if use default value, just pass NULL.
+ *
+ * @return: 0, success; < 0, fail.
+ */
+int route_add(int is_net, uint32_t dst, uint32_t *gw, uint32_t *mask, char *dev);
+
+/*
+ * add route entry. see 'route_add'.
+ */
+int route_del(int is_net, uint32_t dst, uint32_t *gw, uint32_t *mask, char *dev);
